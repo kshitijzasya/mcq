dd3b5d1ccbdc3ec68bc1d7546efa114a086dc966
kshitijzasya <kshtjsharma68@gmail.com>
2024-04-16 19:31:13 +0530

    build error

--- src/app/ui/widgets/news/card.tsx
+++ src/app/ui/widgets/news/card.tsx
@@ -9,11 +9,6 @@ type CardDataProp = {
 
 
 const Card: React.FC<CardDataProp> = (props) => {
-  let element;
-  const handleError = _ => {
-    console.log('element', element)
-    element.src = "/images/cover/cover-01.png";
-  }
 
     return (
         <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-5">
@@ -31,7 +26,7 @@ const Card: React.FC<CardDataProp> = (props) => {
               props.news.map(function(val, key) {
                 return (
                   <Link key={key} href={val.url} className="grid grid-cols-3" target="_blank">
-                    <img className="max-h-32" alt={`news-${key}`} ref={el => element = el} src={val.urlToImage} onError={() => handleError()} width={200} height={130}/>
+                    <img className="max-h-32" alt={`news-${key}`} src={val.urlToImage} width={200} height={130}/>
                     <h5 className="col-span-2 pl-2" title={val.title}><b>{val.title.substring(0, 70)}...</b></h5>
                   </Link>
                 )
